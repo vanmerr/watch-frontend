e627a3ebada1851863e09fca3b5fbe0e8b97a15d
vanmerrcoder <vanmerr020403@gmail.com>
2024-04-12 20:14:11 +0700

    update : setup project

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+const images = {
+    logo: require('./images/logoShoesShop.png'),
+};
+
+export default images;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import style from './Header.module.scss';
+import { Container, Row, Col, Form, FormControl, Button } from 'react-bootstrap';
+
+const cs = classNames.bind(style);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className = {cs('header')}>
+            <div className = {cs('logo')}>
+                
+            </div>
+            <div className = {cs('search')}>
+                
+            </div>
+            <div className = {cs('cart')}></div>
+            <div className = {cs('authen')}></div>
+        </header>
+    );
 }
 
 export default Header;
